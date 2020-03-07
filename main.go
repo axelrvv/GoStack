74f31ce86ff1df99a5cb56e98f9014ec1796e92a
Axel <axelrvv@gmail.com>
2020-03-07 14:31:34 -0400

    Added string stack Added all funcs to add a stack for strings datatype

--- main.go
+++ main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	s "Stack/stack"
-	"fmt"
-	"strconv"
-)
-
 func main() {
-
-	t := s.Ints{X: 6}
-
-	s.Ints.Push(t)
-
-	t.X = 4
-	s.Ints.Push(t)
-
-	t.X = 1
-	s.Ints.Push(t)
-
-	t.X = 9
-	s.Ints.Push(t)
-
-	u := s.Ints{X: 120}
-
-	s.Ints.Pop(t)
-	s.Ints.Push(u)
-	fmt.Println("Top: " + s.Ints.Top(u))
-	fmt.Println("Is it empty? " + strconv.FormatBool(s.Ints.IsEmpty(t)))
-	fmt.Println("Size: " + s.Ints.GetSize(u))
 
 }
--- stack/stack.go
+++ stack/stack.go
@@ -1,6 +1,9 @@
 package stack
 
-import "strconv"
+import (
+	"errors"
+	"strconv"
+)
 
 type stack interface {
 	//Add to stack
@@ -20,17 +23,19 @@ type Ints struct {
 	X int
 }
 
-type strings struct {
-	x string
+//Strings : string data type
+type Strings struct {
+	X string
 }
 
-type structs struct {
-	x struct{}
+//Data : Any data type can be stored here
+type Data struct {
+	X interface{}
 }
 
 var intsArr []Ints
-var stringsArr []strings
-var structsArr []struct{}
+var stringsArr []Strings
+var dataArr []Data
 
 //Push : Add an int to the stack
 func (i Ints) Push() string {
@@ -39,19 +44,10 @@ func (i Ints) Push() string {
 	return "Pushed " + number
 }
 
-//Top : Show the las element inserted in the stack
-func (i Ints) Top() string {
-	length := len(intsArr)
-	number := strconv.Itoa(intsArr[length-1].X)
-	return number
-}
-
-//Pop : Delete the top element inserted in the stack
-func (i Ints) Pop() string {
-	length := len(intsArr)
-	popped := strconv.Itoa(intsArr[length-1].X)
-	intsArr = intsArr[:length-1]
-	return popped
+//Push : Add a string to the stack
+func (i Strings) Push() string {
+	stringsArr = append(stringsArr, i)
+	return stringsArr[len(stringsArr)-1].X
 }
 
 //IsEmpty : returns true is stack is empty else it returns false
@@ -62,8 +58,62 @@ func (i Ints) IsEmpty() bool {
 	return false
 }
 
+//IsEmpty : returns true is stack is empty else it returns false
+func (i Strings) IsEmpty() bool {
+	if len(stringsArr) == 0 {
+		return true
+	}
+	return false
+}
+
+//Top : Show the las element inserted in the stack
+func (i Ints) Top() (string, error) {
+	if Ints.IsEmpty(i) {
+		return "", errors.New("Stack is empty")
+	}
+	length := len(intsArr)
+	number := strconv.Itoa(intsArr[length-1].X)
+	return number, nil
+}
+
+//Top : Show the las element inserted in the stack
+func (i Strings) Top() (string, error) {
+	if Strings.IsEmpty(i) {
+		return "", errors.New("Stack is empty")
+	}
+	top := stringsArr[len(stringsArr)-1].X
+	return top, nil
+}
+
+//Pop : Delete the top element inserted in the stack
+func (i Ints) Pop() (string, error) {
+	if Ints.IsEmpty(i) {
+		return "", errors.New("Stack is empty")
+	}
+	length := len(intsArr)
+	popped := strconv.Itoa(intsArr[length-1].X)
+	intsArr = intsArr[:length-1]
+	return popped, nil
+}
+
+//Pop : Delete the top element inserted in the stack
+func (i Strings) Pop() (string, error) {
+	if Strings.IsEmpty(i) {
+		return "", errors.New("Stack is empty")
+	}
+	popped := stringsArr[len(stringsArr)-1].X
+	stringsArr = stringsArr[:len(stringsArr)-1]
+	return popped, nil
+}
+
 //GetSize : retuns the size of the stack
-func (i Ints) GetSize() string {
+func (i Ints) GetSize() int {
 	size := len(intsArr)
-	return strconv.Itoa(size)
+	return size
+}
+
+//GetSize : returns the size of the stack
+func (i Strings) GetSize() int {
+	size := len(stringsArr)
+	return size
 }
